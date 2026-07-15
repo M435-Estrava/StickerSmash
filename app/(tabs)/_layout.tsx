6f9419f1308ded888947729bd804ab18ea35ad29
Clint Caserial <clintcaserial3@gmail.com>
2026-07-15 13:30:24 +0800

    3rd

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
+import { Tabs } from 'expo-router';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { Tabs } from 'expo-router';
+
 
 export default function TabLayout() {
   return (
